850ef385c631aaf8491796e2d78eaa92d6021408
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-31 14:13:18 -0300

    Aula 01(/07) completada

---
+++ bytebank-heredado/src/Gerente.java
@@ -0,0 +1,17 @@
+
+public class Gerente extends Funcionario {
+
+	private String clave;
+	
+	public void setClave (String clave) {
+		this.clave = clave;
+	}
+	
+	public boolean iniciarSesion(String clave) {
+		return clave == "AluraCursosOnline";
+	}
+	
+	public double getBonificacion() {
+		return this.getSalario();
+	}
+}
